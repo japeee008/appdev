ed1e74d518ff8aaa1443488d550726e755df9632
Jeff <jefflloyd.seloterio@gmail.com>
2025-11-20 02:45:37 +0800

    updated category entity

--- src/main/java/com/appdev/altf4/seloteriog5/repository/CategoryRepository.java
+++ src/main/java/com/appdev/altf4/seloteriog5/repository/CategoryRepository.java
@@ -1,7 +1,9 @@
 package com.appdev.altf4.seloteriog5.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.appdev.altf4.seloteriog5.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
