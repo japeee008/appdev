6fd727b5b0ef4ba672e8088e780ee499a3acfb15
Jeff <jefflloyd.seloterio@gmail.com>
2025-11-20 02:56:59 +0800

    updated category controller

--- src/main/java/com/appdev/altf4/seloteriog5/controller/CategoryController.java
+++ src/main/java/com/appdev/altf4/seloteriog5/controller/CategoryController.java
@@ -1,25 +1,47 @@
 package com.appdev.altf4.seloteriog5.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
 import com.appdev.altf4.seloteriog5.entity.Category;
 import com.appdev.altf4.seloteriog5.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
+@CrossOrigin(origins = "http://localhost:8080") // change port to your React dev port
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
+    // CREATE
     @PostMapping
     public Category addCategory(@RequestBody Category category) {
-        return categoryService.saveCategory(category);
+        return categoryService.createCategory(category);
     }
 
+    // READ ALL
     @GetMapping
     public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
+    }
+
+    // READ ONE
+    @GetMapping("/{id}")
+    public Category getCategory(@PathVariable Long id) {
+        return categoryService.getCategoryById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        return categoryService.updateCategory(id, category);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
     }
 }
